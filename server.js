e81ed3e32d6eb09de08ee31ef0eb7658de0fa73f
yamamoto_k <19276@toba-cmt.ac.jp>
2022-08-01 01:03:20 +0900

    同じ単語を送信不可に
    server.js内で単語を記録する配列を作成
    includesで入力単語と照会して弾く　実装

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 
 let previousWord = "しりとり";
+let WordList = [];
+WordList.push(previousWord);
 
 console.log("Listening on http://localhost:8000");
 
@@ -12,6 +14,7 @@ serve(async (req) => {
 
     if (req.method === "GET" && pathname === "/shiritori") {
         return new Response(previousWord);
+        
     }
     
     if (req.method === "POST" && pathname === "/shiritori") {
@@ -19,13 +22,18 @@ serve(async (req) => {
         const requestJson = await req.json();
         const nextWord = requestJson.nextWord;
 
-        if (
-            nextWord.length > 0 &&
-            previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)
-          ) {
-            return new Response("前の単語に続いていません。", { status: 400 });
-          }
-
+        if (nextWord.length > 0 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0))
+        {
+          return new Response("前の単語に続いていません。", { status: 400 });
+        }
+      
+        else if (WordList.includes(nextWord) == true)
+        {
+          return new Response("その単語はすでに使われています。", { status: 400 });
+        }
+      
+      WordList.push(nextWord);
+      console.log(WordList);
         previousWord = nextWord;
         return new Response(previousWord);
       }
