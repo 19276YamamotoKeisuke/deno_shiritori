ee49beb1832db695455e09b510c23952bb1fd57b
yamamoto_k <19276@toba-cmt.ac.jp>
2022-07-31 15:53:23 +0900

    deno_shiritori/public変更

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ serve(async (req) => {
       }
 
     return serveDir(req, {
-        fsRoot: "deno_shiritori/public",
+        fsRoot: "public",
         urlRoot: "",
         showDirListing: true,
         enableCors: true,
