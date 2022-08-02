bc977646686bbae5d682297783344dc15bcdbf7b
yamamoto_k <19276@toba-cmt.ac.jp>
2022-08-03 00:44:47 +0900

    進捗
    同じ単語を入力できないようにする
    最初の単語がランダムに決まるようにする
    「ん」で終わる単語が入力されたらゲームを終了する
    ひらがな以外を入力できないようにする
    CSS でオシャレな見た目にする
    しりとりの単語の履歴を表示する
    最初からやり直せるようにリセット機能をつける
    vue.js使ってみた(単語履歴に)

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
 
 import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 
-let previousWord = "しりとり";
+const _chars = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわ';//'を','ん' を除く
+let previousWord = "";
+previousWord += _chars.charAt(Math.floor(Math.random() * _chars.length));
 let WordList = [];
 WordList.push(previousWord);
 
@@ -20,9 +22,9 @@ serve(async (req) => {
     if (req.method === "POST" && pathname === "/shiritori") {
 
         const requestJson = await req.json();
-        const nextWord = requestJson.nextWord;
+        let nextWord = requestJson.nextWord;
 
-        if (nextWord.length > 0 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0))
+        if (nextWord.length > 0 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0) && nextWord != "reset")
         {
           return new Response("前の単語に続いていません。", { status: 400 });
         }
@@ -30,6 +32,26 @@ serve(async (req) => {
         else if (WordList.includes(nextWord) == true)
         {
           return new Response("その単語はすでに使われています。", { status: 400 });
+        }
+        
+        else if (nextWord.charAt(nextWord.length - 1) === "ん")
+        {
+          return new Response("単語の末尾が「ん」になっています。", { status: 400 });
+        }
+        else if (nextWord.match(/^[ぁ-んー　]+$/))
+        {}
+        else if (nextWord == "reset")
+        {
+          nextWord = "";
+          nextWord += _chars.charAt(Math.floor(Math.random() * _chars.length));
+          WordList.length = 0;
+          // app.words_history
+          // nextWordへランダムな単語を入力＝疑似的なリロード+WordListリセット
+          // WordList.push("error")
+          console.log(WordList)
+        }  
+        else {
+          return new Response("ひらがな以外は入力できません。(空白含む)", { status: 400 });
         }
       
       WordList.push(nextWord);
@@ -39,10 +61,14 @@ serve(async (req) => {
       }
 
     return serveDir(req, {
-      fsRoot: "deno_shiritori/public",
+      fsRoot: "public",
       // push時に変更しないとdeployweb上でエラー
         urlRoot: "",
         showDirListing: true,
         enableCors: true,
       });
 });
+
+// function _save() {
+//   return WordList;
+// }
